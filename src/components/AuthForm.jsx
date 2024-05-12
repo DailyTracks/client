7167639d48227b4e382f139323021b47ccec66c0
KNU-K <knukang334@gmail.com>
2024-05-12 13:12:05 +0900

    [feat/auth-v2] 로그인및 회원 가입 1차 수정

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -43,40 +43,149 @@ import { Form, Link, useSearchParams } from "react-router-dom";
 
 import classes from "../styles/AuthForm.module.css";
 import NaverLogin from "./Naver";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function AuthForm() {
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
-
+  const [provider, setProvider] = useState("");
+  const [id, setId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const btnStyle = {
     display: isLogin ? "block" : "none",
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchParams.get("mode") !== "signup") return;
 
+      try {
+        const res = await axios.get("http://localhost:8080/api/auth", {
+          withCredentials: true,
+        });
+        //TODO: 오류 처리해주세요
+
+        const { id, oauth_provider, username, email } = res.data.user;
+        console.log(id, oauth_provider, username, email);
+        setProvider(oauth_provider);
+        setName(username);
+        setEmail(email);
+        setId(id);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const createProfile = async (e) => {
+    const res = await axios.post(
+      `http://localhost:8080/api/user/${id}/profile`,
+      {
+        userId: userId,
+        password: password,
+      }
+    );
+    console.log(res.data);
+  };
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
-        </p>
-        {!isLogin && (
-          <p>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" name="username" required />
-          </p>
+
+        {!isLogin ? (
+          <>
+            <p>
+              <label htmlFor="provider">Provider</label>
+              <input
+                id="provider"
+                value={provider}
+                type="text"
+                name="provider"
+                readOnly
+              />
+            </p>
+            <p>
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                value={name}
+                type="text"
+                name="username"
+                readOnly
+              />
+            </p>
+            <p>
+              <label htmlFor="email">Email</label>
+              <input
+                value={email}
+                id="email"
+                type="email"
+                name="email"
+                readOnly
+              />
+            </p>
+            <p>
+              <label htmlFor="userId">UserId</label>
+              <input
+                id="userId"
+                type="text"
+                name="userId"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)}
+                required
+              />
+            </p>
+
+            <p>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </p>
+            <button onClick={(e) => createProfile(e)}>확인</button>
+          </>
+        ) : (
+          <>
+            <p>
+              {/* 추후 userid와 email 동시 운용 */}
+              <label htmlFor="userId">Email</label>
+              <input
+                id="userId"
+                type="text"
+                name="userId"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)}
+                required
+              />
+            </p>
+
+            <p>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </p>
+
+            <NaverLogin />
+          </>
         )}
-        <p>
-          <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
-        </p>
+
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
-            {isLogin ? "회원가입 하러가기" : "로그인"}
-          </Link>
           <button style={btnStyle}>{isLogin ? "로그인" : "회원가입"}</button>
         </div>
-        <NaverLogin />
       </Form>
     </>
   );
--- src/components/Naver.jsx
+++ src/components/Naver.jsx
@@ -3,24 +3,13 @@ import { useEffect } from "react";
 function Naver() {
   const { naver } = window;
 
-  const initializeNaverLogin = () => {
-    const naverLogin = new naver.LoginWithNaverId({
-      clientId: process.env.REACT_APP_NAVER_CLIENT_ID,
-      callbackUrl: process.env.REACT_APP_NAVER_CALLBACK_URL,
-      isPopup: true, // popup 형식으로 띄울것인지 설정
-      loginButton: { color: "green", type: 2, height: "50" }, //버튼의 스타일, 타입, 크기를 지정
-    });
-    naverLogin.init();
+  const handlerLogin = (e) => {
+    window.location.href = "http://localhost:8080/api/auth/join/naver";
   };
-
-  useEffect(() => {
-    initializeNaverLogin();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
-      <div id="naverIdLogin" />
+      <button onClick={(e) => handlerLogin(e)}>네이버로 간편가입하기</button>
     </>
   );
 }
